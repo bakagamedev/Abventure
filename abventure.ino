49c1ebbbad5461bc403e3194e8ab1f5b7a5a0d28
bakagamedev <noel.abercrombie@hotmail.com>
2017-03-15 01:04:58 +0000

    Stripped everything out

--- abventure.ino
+++ abventure.ino
@@ -1,7 +1,7 @@
 /*
 
 */
-#include <Arduboy.h>
+#include <Arduboy2.h>
 #include "system.h"
 #include "game.h"
 #include "rooms.h"
@@ -9,46 +9,16 @@
 #include "sprites.h"
 #include "player.h"
 
-System ab = System();
+Arduboy2 ab = Arduboy2();
 Player player = Player(ab);
 Game game = Game(ab,player);
 
 int main(void)
 {
 	init();
-
 	#ifdef USBCON
     	USBDevice.attach();
 	#endif
-
 	ab.begin();
-
-	bool ready = false;
-	uint8_t map = 0;
-
-	while(!ready)
-	{
-
-		if(ab.nextFrame())
-		{
-			/*
-	  		if (ab.pressed(BUTTON_UP))	 {	if (map>0) 	map--;	}
-	  		if (ab.pressed(BUTTON_DOWN)) {	if (map<10) map++;	}
-			*/
-			ab.fillScreen(1);
-			ab.drawPlayfield(mapGraphics[map]);
-			ab.display();
-
-			map++;
-			delay(1000);
-		}
-	}
-
-	while(1)
-	{
-		if(ab.nextFrame())
-		{
-			game.frame();
-		}
-	}
+	
 }
